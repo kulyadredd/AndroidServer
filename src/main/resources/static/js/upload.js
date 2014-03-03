6fbaff37f76ee819b7d1d942802a730b40be6fa0
kulyadredd <kyluadredd08@rambler.ru>
2014-03-03 21:45:45 +0200

    file_value

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -44,10 +44,17 @@ app.controller('ServerFile', ['$scope','$http', '$fileUpload', function ($scope,
 
     $scope.$watch('ncateg', function() {
     	if ($scope.ncateg)
+            $scope.allValue.length = 0;
     		$scope.getImages();
+            $scope.getSounds();
+            $scope.getPathTxt();
     });
     
     $scope.fullpathIMG = new Array();
+    $scope.fullpathMID = new Array();
+    $scope.txtpath = new Array();
+    $scope.valueFile = new Array();
+    $scope.allValue = new Array();
     
     $scope.getImages = function(){
         $http.get("info/images/"+$scope.ncateg).success(
@@ -57,6 +64,46 @@ app.controller('ServerFile', ['$scope','$http', '$fileUpload', function ($scope,
                     $scope.fullpathIMG[i]="/images/"+$scope.ncateg+"/"+data[i];
             }
         );
+    }
+
+    $scope.getSounds = function(){
+        $http.get("info/sounds/"+$scope.ncateg).success(
+            function(data){
+                $scope.fullpathMID.length = 0;
+                for (var i=0; i<data.length; i++)
+                    $scope.fullpathMID[i]="/sounds/"+$scope.ncateg+"/"+data[i];
+            }
+        );
+    }
+
+    $scope.getPathTxt = function(){
+       $http.get("info/text/"+$scope.ncateg).success(
+            function (data){ 
+                if (data!="null") {
+                $scope.txtpath.length = 0;    
+                for (var i=0; i < data.length ; i++)
+                    $scope.txtpath[i]="/text/"+$scope.ncateg+"/"+data[i];
+                $scope.getValueTxt($scope.txtpath);
+            }
+        }
+        );
+    }
+
+    $scope.getValueTxt = function(txtpath){
+        $scope.valueFile.length=0;
+        var c=0;
+            for(var i=0; i<txtpath.length;i++){
+            $http.get(txtpath[i]).success(
+                function(data){
+                    $scope.valueFile[c] = data;
+                    c++;
+                    if (c==txtpath.length){
+                        for (var i=0; i<$scope.fullpathIMG.length; i++) 
+                            $scope.allValue.push({IMG : $scope.fullpathIMG[i],  MID : $scope.fullpathMID[i], TXT : $scope.valueFile[i]})
+                    }
+                 }
+            )
+        }
     }  
     
     $scope.uploadFile = function(){
