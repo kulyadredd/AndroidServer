23da0360baf39387ac21838592b73690875d3adb
kulyadredd <kyluadredd08@rambler.ru>
2014-03-08 18:22:27 +0200

    two_watch_cat

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,74 +1,86 @@
 ﻿var app = angular.module('MyAngularApp', ['ngSanitize', 'mgcrea.ngStrap'], function() {})
 
 app.controller('ServerFile', ['$scope','$http', function ($scope, $http) {
-
-    $http.get("info").success(
+		
+	
+	$http.get("info").success(
         function(data){
             $scope.categories=data;
-            $scope.ncateg = '';
+            $scope.incat = '';
         }
     );
 
     $scope.addcat = function() {
-        $scope.categories.push($scope.ncateg);
-        $http.get("/addcat?newcategory="+$scope.ncateg);
+        $scope.categories.push($scope.incat);
+        $http.get("/addcat?newcategory="+$scope.incat).success(function(data){
+		    $scope.ncateg = $scope.incat;
+		    $scope.allValues = {};
+		    $scope.getImages();
+		    $scope.getSounds();
+		    $scope.getPathTxt();
+        });
+        $scope.visible="false";
     };
 
     $scope.allValues = {};
 
-    $scope.$watch('ncateg', function() {
-        for (var i = 0; i<$scope.categories.length; i++) {
-            if($scope.ncateg==$scope.categories[i]){
-            	$scope.allValues = {};
-                $scope.getImages();
-                $scope.getSounds();
-                $scope.getPathTxt();
-                $scope.visible="false";
-                break;
-            }else $scope.visible="true";
-        };
-        if ($scope.ncateg=='')
-        	$scope.visible="false";
-    });
+    $scope.$watch('incat', function() {
+    	    if(!$scope.categories)
+    	        return;
+    	    for (var i = 0; i<$scope.categories.length; i++) 
+    	        if($scope.incat==$scope.categories[i]){
+    	            $scope.ncateg = $scope.incat;
+    	            break;
+    	        } else $scope.visible="true";
+    	 	if($scope.incat == '')
+    	 		$scope.visible="false";
+    	 });
+
+    	$scope.$watch('ncateg', function(){
+    		if($scope.ncateg){
+	    	    $scope.allValues = {};
+	    	    $scope.getImages();
+	    	    $scope.getSounds();
+	    	    $scope.getPathTxt();
+	    	 	$scope.visible="false";
+    		}
+    	});
     
     $scope.getImages = function(){
-        $http.get("info/images/"+$scope.ncateg).success(function (data){
-            if (data!=null) {
-                for (var i=0; i < data.length ; i++){
-                	var key = "/"+$scope.ncateg+"/"+data[i].replace('.png','');
-                	if (typeof $scope.allValues[key] !== 'object')
-                		$scope.allValues[key] = {};
-                    $scope.allValues[key].IMG = "/images/"+$scope.ncateg+"/"+data[i];
-                }
-            };
+    $http.get("info/images/"+$scope.ncateg).success(function (data){
+            for (var i=0; i < data.length ; i++){
+            	var key = "/"+$scope.ncateg+"/"+data[i].replace('.png','');
+            	if (typeof $scope.allValues[key] !== 'object')
+            		$scope.allValues[key] = {};
+                $scope.allValues[key].IMG = "/images/"+$scope.ncateg+"/"+data[i];
+                $scope.visible = "false";
+            }
         });
     }
 
     $scope.getSounds = function(){
-        $http.get("info/sounds/"+$scope.ncateg).success(function(data){
-            if (data!=null) {
-                for (var i=0; i<data.length; i++){
-                	var key = "/"+$scope.ncateg+"/"+data[i].replace('.mp3','');
-                	if (typeof $scope.allValues[key] !== 'object')
-                		$scope.allValues[key] = {};
-                	$scope.allValues[key].SOUND = "/sounds/"+$scope.ncateg+"/"+data[i];
-                }
-            };
+    $http.get("info/sounds/"+$scope.ncateg).success(function(data){
+            for (var i=0; i<data.length; i++){
+            	var key = "/"+$scope.ncateg+"/"+data[i].replace('.mp3','');
+            	if (typeof $scope.allValues[key] !== 'object')
+            		$scope.allValues[key] = {};
+            	$scope.allValues[key].SOUND = "/sounds/"+$scope.ncateg+"/"+data[i];
+            	$scope.visible = "false";
+            }
         });
     }
 
     $scope.getPathTxt = function(){
-       $http.get("info/text/"+$scope.ncateg).success(function (data){
-           if (data!=null) {                
-                for (var i=0; i < data.length ; i++){
-                    $http.get("/text/"+$scope.ncateg+"/"+data[i]).success(function(data, code, f, status){
-                    	var key = status.url.replace('/text', '').replace('.txt','');
-                    	if (typeof $scope.allValues[key] !== 'object')
-                    		$scope.allValues[key] = {};
-                    	$scope.allValues[key].TXT = data;
-                    });
-                }
-            };
+       $http.get("info/text/"+$scope.ncateg).success(function (data){             
+            for (var i=0; i < data.length ; i++){
+                $http.get("/text/"+$scope.ncateg+"/"+data[i]).success(function(data, code, f, status){
+                	var key = status.url.replace('/text', '').replace('.txt','');
+                	if (typeof $scope.allValues[key] !== 'object')
+                		$scope.allValues[key] = {};
+                	$scope.allValues[key].TXT = data;
+                	$scope.visible = "false";
+                });
+            }
         });
     }
 }]);
