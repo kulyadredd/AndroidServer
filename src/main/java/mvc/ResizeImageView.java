1fe51028b4656e6ba87270e57078289f7d9bf273
kulyadredd <kyluadredd08@rambler.ru>
2014-03-25 21:06:12 +0200

    resize

---
+++ src/main/java/mvc/ResizeImageView.java
@@ -0,0 +1,50 @@
+package mvc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.apache.commons.io.FilenameUtils;
+
+import util.MimeType;
+
+public class ResizeImageView implements View {
+
+	private Path path;
+	private InputStream in;
+
+	public ResizeImageView(String path, InputStream in) {
+		this.path = Paths.get(path);
+		this.in = in;
+		// TODO Auto-generated constructor stub
+	}
+
+	public void view(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		String ext = FilenameUtils.getExtension(path.getFileName().toString());
+		response.setContentType(MimeType.getByFileExtension(ext).getMimeType());
+		resizeFileShow(response.getOutputStream());
+	}
+
+	private void resizeFileShow(OutputStream out) throws IOException {
+		try {
+			int ch = 0;
+			byte[] buffer = new byte[4096];
+			while ((ch = in.read(buffer)) != -1) {
+				out.write(buffer, 0, ch);
+				Arrays.fill(buffer, (byte) 0);
+			}
+		} finally {
+			if (in != null)
+				in.close();
+		}
+	}
+
+}
---
+++ src/main/java/webs/ImageFiles.java
@@ -0,0 +1,89 @@
+package webs;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+
+import mvc.PathParser;
+import mvc.ResizeImageView;
+import mvc.View;
+
+@SuppressWarnings("serial")
+public class ImageFiles extends DataFiles {
+
+	private int ratiowigth;
+	private int ratioheigth;
+	private BufferedImage originalImage;
+
+	public ImageFiles(String root) {
+		super(root);
+		// TODO Auto-generated constructor stub
+	}
+
+	public View get(HttpServletRequest request, PathParser pathInfo)
+			throws Exception {
+
+		String category = pathInfo.cutNext();
+		String name = pathInfo.cutNext();
+		if (name.contains(".png") || name.contains(".jpg"))
+			return super.get(request, pathInfo);
+		else {
+			int wigth = Integer.parseInt(name);
+			int heigth = Integer.parseInt(pathInfo.cutNext());
+			String filepath = root + "/images/" + category + File.separator + pathInfo.cutNext();
+			originalImage = ImageIO.read(new File(filepath));
+			if (originalImage.getWidth() > originalImage.getHeight())
+				normalResolutionImage(wigth, heigth);
+			else
+				converseResolutioImage(wigth, heigth);
+			return new ResizeImageView(filepath, resizeFile());
+		}
+	}
+
+	private void converseResolutioImage(int wigth, int heigth)
+			throws IOException {
+		if (wigth > heigth) {
+			ratioheigth = wigth;
+			Double kof = (double) (wigth * 100 / originalImage.getHeight());
+			ratiowigth = (int) (originalImage.getWidth() * kof / 100);
+		} else {
+			ratioheigth = heigth;
+			Double kof = (double) (heigth * 100 / originalImage.getHeight());
+			ratiowigth = (int) (originalImage.getWidth() * kof / 100);
+		}
+	}
+
+	private void normalResolutionImage(int wigth, int heigth)
+			throws IOException {
+		if (wigth > heigth) {
+			ratiowigth = wigth;
+			Double kof = (double) (wigth * 100 / originalImage.getWidth());
+			ratioheigth = (int) (originalImage.getHeight() * kof / 100);
+		} else {
+			ratiowigth = heigth;
+			Double kof = (double) (heigth * 100 / originalImage.getWidth());
+			ratioheigth = (int) (originalImage.getHeight() * kof / 100);
+		}
+	}
+
+	private InputStream resizeFile() throws IOException {
+		int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+		BufferedImage resizedImage = new BufferedImage(ratiowigth, ratioheigth, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, ratiowigth, ratioheigth, null);
+		g.dispose();
+		ByteArrayOutputStream resizedImageOS = new ByteArrayOutputStream();
+		ImageIO.write(resizedImage, "png", resizedImageOS);
+		InputStream in = new ByteArrayInputStream(resizedImageOS.toByteArray());
+		return in;
+
+	}
+
+}
