7363617daaf33224d41157288f125d33dcb79b82
Arsen Kostenko <arsen.kostenko@sonydadc.com>
2014-04-03 17:27:20 -0700

    TODO and bug fixes

--- src/main/java/mvc/ErrorView.java
+++ src/main/java/mvc/ErrorView.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ErrorView implements View {
     
     public static final ErrorView SERVER_ERROR_GENERIC = new ErrorView(500);
@@ -14,14 +16,22 @@ public class ErrorView implements View {
     public static final View NOT_FOUND_GENERIC = new ErrorView(404);
     
     private int statusCode;
+    private String message;
 
     public ErrorView(int statusCode) {
         this.statusCode = statusCode;
+    }
+    
+    public ErrorView(int stausCode, String message){
+        statusCode = stausCode;
+        this.message = message;
     }
 
     @Override
     public void view(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setStatus(statusCode);
+        if (StringUtils.isNotBlank(message))
+            response.getOutputStream().write(message.getBytes());
     }
 
 }
